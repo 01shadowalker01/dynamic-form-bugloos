12b86e526250f6d979f093458b41c746c12e2c4a
shadowalker <saeed.bayat@arusha.ir>
2021-11-28 13:13:49 +0330

    Add two new form fields

--- projects/dynamic-form-builder/src/lib/dynamic-form-builder.component.ts
+++ projects/dynamic-form-builder/src/lib/dynamic-form-builder.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FieldConfig } from '../models/field-config';
+import { FormConfig } from '../models/form-config';
 
 @Component({
   selector: 'lib-dynamic-form-builder',
-  template: `
-    <p>
-      dynamic-form-builder works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>dynamic-form-builder works!</p> `,
+  styles: [],
 })
 export class DynamicFormBuilderComponent implements OnInit {
+  @Input() formConfig!: FormConfig | FieldConfig[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- projects/dynamic-form-builder/src/lib/dynamic-form-builder.module.ts
+++ projects/dynamic-form-builder/src/lib/dynamic-form-builder.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { DynamicFormBuilderComponent } from './dynamic-form-builder.component';
+import { InputComponent } from './form-fields/input/input.component';
+import { TextAreaComponent } from './form-fields/text-area/text-area.component';
 
 
 
 @NgModule({
   declarations: [
-    DynamicFormBuilderComponent
+    DynamicFormBuilderComponent,
+    InputComponent,
+    TextAreaComponent
   ],
   imports: [
   ],
