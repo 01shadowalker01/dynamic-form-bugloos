5ac90e2e688ad74b65a499b0e293a715caea6122
shadowalker <saeed.bayat@arusha.ir>
2021-11-28 13:20:20 +0330

    Replace index with public-api, prettier conf added

---
+++ projects/dynamic-form-builder/index.ts
@@ -0,0 +1,5 @@
+export * from "./src/lib/dynamic-form-builder.module";
+export * from "./src/models/field-config";
+export * from "./src/models/form-config";
+export * from "./src/models/form-field";
+export * from "./src/models/template-options";
--- projects/dynamic-form-builder/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dynamic-form-builder
- */
-
-export * from './lib/dynamic-form-builder.service';
-export * from './lib/dynamic-form-builder.component';
-export * from './lib/dynamic-form-builder.module';
