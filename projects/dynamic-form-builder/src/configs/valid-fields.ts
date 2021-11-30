ebb4e5e97a4cd9419aa6df5b14d6d3394e5251a8
shadowalker <saeed.bayat@arusha.ir>
2021-11-30 17:05:32 +0330

    Add VALID_FIELDS list
    now if given config has any non-valid types, an error will be thrown

---
+++ projects/dynamic-form-builder/src/configs/valid-fields.ts
@@ -0,0 +1,7 @@
+export const VALID_FIELDS: string[] = [
+  "input",
+  "textarea",
+  "date-picker",
+  "date-range",
+  "select",
+]
--- projects/dynamic-form-builder/src/lib/dynamic-form-builder.component.ts
+++ projects/dynamic-form-builder/src/lib/dynamic-form-builder.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
+import { VALID_FIELDS } from "../configs/valid-fields";
 import { FormConfig } from "../models/form-config";
 
 @Component({
@@ -6,10 +13,26 @@ import { FormConfig } from "../models/form-config";
   templateUrl: "./dynamic-form-builder.component.html",
   styleUrls: ["./dynamic-form-builder.component.scss"],
 })
-export class DynamicFormBuilderComponent implements OnInit {
+export class DynamicFormBuilderComponent implements OnInit, OnChanges {
   @Input() formConfig!: FormConfig;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["formConfig"]) {
+      this.checkConfigForValidation(this.formConfig);
+    }
+  }
+
+  checkConfigForValidation(config: FormConfig) {
+    if (config.fields) {
+      config.fields.forEach(field => {
+        if (VALID_FIELDS.indexOf(field.type) == -1) {
+          throw new Error(`Field type '${field.type}' is not a valid type'`);
+        }
+      });
+    }
+  }
 }
