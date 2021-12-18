017487aa04ef4223e79979790bc95519df27244e
shadowalker <01shadowalker01@gmail.com>
2021-12-18 15:52:40 +0330

    feat: add formControl to input
    implement ControlValueAccessor in FormField to make it possible ->
    to use formContol directive on FormField childs

--- projects/dynamic-form-builder/src/lib/form-fields/input/input.component.ts
+++ projects/dynamic-form-builder/src/lib/form-fields/input/input.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from "@angular/core";
+import { NG_VALUE_ACCESSOR } from "@angular/forms";
 import { FormField } from "../../../models/form-field";
 
 @Component({
   selector: "lib-input",
   templateUrl: "./input.component.html",
   styleUrls: ["./input.component.scss"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: InputComponent,
+      multi: true,
+    },
+  ],
 })
 export class InputComponent extends FormField implements OnInit {
   constructor() {
--- projects/dynamic-form-builder/src/models/form-field.ts
+++ projects/dynamic-form-builder/src/models/form-field.ts
@@ -1,12 +1,35 @@
 import { Directive, Input } from "@angular/core";
+import { ControlValueAccessor } from "@angular/forms";
 import { TemplateOptions } from "projects/dynamic-form-builder";
 import { FieldConfig } from "./field-config";
 
 @Directive()
-export class FormField {
+export class FormField implements ControlValueAccessor {
   @Input() config!: FieldConfig;
+
+  protected value: any;
+  protected _onChange!: (value: any) => void;
+  protected _onTouched!: () => void;
 
   public get to(): TemplateOptions {
     return this.config.templateOptions || {};
+  }
+
+  writeValue(value: any) {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any) {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this._onTouched = fn;
+  }
+
+  onValueChange(value: any) {
+    this.value = value;
+    this._onChange(value);
+    this._onTouched();
   }
 }
