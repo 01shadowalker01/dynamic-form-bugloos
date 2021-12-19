9bb268547fc6fea95f2d9cbe24aab11f4b51bfda
shadowalker <01shadowalker01@gmail.com>
2021-12-19 21:20:24 +0330

    feat(demo): add grouping possiblity for sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,17 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  navList = [
-    { route: "/get-started", title: "Get Started" },
-    { route: "/components", title: "Components" },
+  navGroupList = [
+    {
+      title: "Form fields",
+      navList: [
+        { route: "/get-started", title: "Get Started" },
+        { route: "/components", title: "Components" },
+      ],
+    },
+    {
+      title: "Form management",
+      navList: [],
+    },
   ];
 }
