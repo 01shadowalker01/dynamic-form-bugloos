aa3198673f998de6e57169f4c3b5d850f4a7c3b9
shadowalker <01shadowalker01@gmail.com>
2021-12-19 20:21:33 +0330

    fix: rename foolder

--- projects/dynamic-form-builder/index.ts
+++ projects/dynamic-form-builder/index.ts
@@ -1,4 +1,4 @@
-export * from "./src/lib/dynamic-form-builder.module";
+export * from "./src/form-builder/dynamic-form-builder.module";
 export * from "./src/models/field-config";
 export * from "./src/models/form-config";
 export * from "./src/models/form-field";
