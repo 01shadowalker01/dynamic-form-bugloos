2c1c5326071e3e0087b400599b16d4ec02404355
shadowalker <01shadowalker01@gmail.com>
2021-12-21 10:59:46 +0330

    feat: add some APIs to access elements from outside

--- projects/dynamic-form-builder/index.ts
+++ projects/dynamic-form-builder/index.ts
@@ -1,6 +1,9 @@
 export * from "./src/form-builder/dynamic-form-builder.module";
+export * from "./src/form-builder/dynamic-form-builder.component";
+
 export * from "./src/models/field-config";
 export * from "./src/models/form-config";
 export * from "./src/models/form-field";
 export * from "./src/models/template-options";
-export * from "./src/form-manager/form-manager.module";
+
+export * from "./src/form-manager";
